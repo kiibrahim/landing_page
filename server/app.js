4b97f1ae6ee63a5c213a303fd1856ad3ed0ca9ca
ibrahimmunna <ibrahimmunna059@gmail.com>
2022-08-16 12:27:27 +0200

    Create server side

--- server/app.js
+++ server/app.js
@@ -1,1 +1,15 @@
-console.log("Hi!")
+const express = require('express')
+//const bodyParser = require('body-parser')
+
+const app = express()
+
+//app.use(bodyParser.json())
+
+app.get('/',(req,res)=>{
+res.send({
+    m:"Sa"
+})
+})
+
+
+app.listen(process.env.PORT || 900)
